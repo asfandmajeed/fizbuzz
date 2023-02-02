0c344f5e8672c28972de3eda4df225fe0e592eb1
asfandmajeed <123664168+asfandmajeed@users.noreply.github.com>
2023-02-01 23:52:52 -0400

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+for i in range(100):
+	if(i%3==0 and i%5==0):
+		print("fizzbuzz")
+	elif(i%3==0):
+		print("fiz")
+	elif(i%5==0):
+		print("buzz")
+	else:
+		print(i)
---
+++ task100.py
@@ -0,0 +1,7 @@
+import os
+
+
+inputFilename = input("enter the filename")
+inputFile=open(inputFilename, "r")
+
+print("pening file",inputFilename, "for reading")
